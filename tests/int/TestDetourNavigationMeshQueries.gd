f15d2f2b52c82ad8e0c75433aaafa2419a0bceab
Pawel Lampe <pawel.lampe@gmail.com>
2022-05-20 23:58:13 +0200

    Add navmesh tests

--- tests/int/TestDetourNavigationMeshQueries.gd
+++ tests/int/TestDetourNavigationMeshQueries.gd
@@ -1,5 +1,7 @@
 extends "res://addons/gut/test.gd"
 
+const EPSILON3 = Vector3(0.01, 0.01, 0.01)
+
 
 func test_server_is_present():
 	assert_true(AdvancedNavigationServer3D is Node)
@@ -28,5 +30,30 @@ func test_detour_navigation_mesh_built():
 	# navmesh:
 	var navmesh = AdvancedNavigationServer3D.create_empty_detour_navigation_mesh()
 	assert_true(navmesh != null)
-	# TODO: likely I need to define gdns for it
-	# assert_true(navmesh.build_from_input_geometry(input_geometry, recast_config, detour_config))
+	assert_true(navmesh.build_from_input_geometry(input_geometry, recast_config, detour_config))
+
+
+func test_get_closest_point_failed():
+	var navmesh = AdvancedNavigationServer3D.create_empty_detour_navigation_mesh()
+	assert_eq(navmesh.get_closest_point(Vector3.ZERO), Vector3.INF)
+
+
+func test_get_closest_point_succeeded():
+	# input:
+	var input_geometry = AdvancedNavigationServer3D.create_empty_input_geometry()
+	var plane_mesh = PlaneMesh.new()
+	plane_mesh.size = Vector2(10, 10)
+	input_geometry.add_resources([plane_mesh])
+
+	# config:
+	var recast_config = AdvancedNavigationServer3D.create_empty_recast_polygon_mesh_config()
+	var detour_config = AdvancedNavigationServer3D.create_empty_detour_navigation_mesh_config()
+
+	# navmesh:
+	var navmesh = AdvancedNavigationServer3D.create_empty_detour_navigation_mesh()
+	navmesh.build_from_input_geometry(input_geometry, recast_config, detour_config)
+	assert_almost_eq(navmesh.get_closest_point(Vector3(0, 5, 0)), Vector3(0, 0.2, 0), EPSILON3)
+	assert_almost_eq(
+		navmesh.get_closest_point(Vector3(-10, 0, -10)), Vector3(-4.1, 0.2, -4.1), EPSILON3
+	)
+	assert_almost_eq(navmesh.get_closest_point(Vector3(10, 0, 10)), Vector3(4, 0.2, 4), EPSILON3)
